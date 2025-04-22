e195d0aeaffe2e8a43deff2ad53dd707edc72d87
sekinah <sekinahdeleoladepo@gmail.com>
2025-04-22 21:52:32 +0100

    Add project files.

---
+++ AlgorithmAssignment22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAssignment22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MultiplesOfThreeAndFive(10));
+            
+        }
+
+        public  static int MultiplesOfThreeAndFive(int num)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < num; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+
+
+    class MyClass
+    {
+
+    }
+}
